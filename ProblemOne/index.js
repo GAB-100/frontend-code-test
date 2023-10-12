932dfaa7dc904ae0f569bec2b83a05a8de611b7f
GAllenB <kelvineddyimache@gmail.com>
2023-10-12 14:35:26 -0400

    chore: update function to return a as a flattened array

--- ProblemOne/index.js
+++ ProblemOne/index.js
@@ -1,65 +1,73 @@
 var arr = [
   {
-    'guest_type': 'crew',
-    'first_name': 'Marco',
-    'last_name': 'Burns',
-    'guest_booking': {
-        'room_no': 'A0073',
-        'some_array': [7,2,4]
+    guest_type: "crew",
+    first_name: "Marco",
+    last_name: "Burns",
+    guest_booking: {
+      room_no: "A0073",
+      some_array: [7, 2, 4],
     },
   },
   {
-    'guest_type': 'guest',
-    'first_name': 'John',
-    'last_name': 'Doe',
-    'guest_booking': {
-        'room_no': 'C73',
-        'some_array': [1,3,5,2,4,3]
+    guest_type: "guest",
+    first_name: "John",
+    last_name: "Doe",
+    guest_booking: {
+      room_no: "C73",
+      some_array: [1, 3, 5, 2, 4, 3],
     },
   },
   {
-    'guest_type': 'guest',
-    'first_name': 'Jane',
-    'last_name': 'Doe',
-    'guest_booking': {
-        'room_no': 'C73',
-        'some_array': [1,3,5,2,4,3]
+    guest_type: "guest",
+    first_name: "Jane",
+    last_name: "Doe",
+    guest_booking: {
+      room_no: "C73",
+      some_array: [1, 3, 5, 2, 4, 3],
     },
   },
   {
-    'guest_type': 'guest',
-    'first_name': 'Albert',
-    'last_name': 'Einstein',
-    'guest_booking': {
-        'room_no': 'B15',
-        'some_array': [2,5,6,3]
+    guest_type: "guest",
+    first_name: "Albert",
+    last_name: "Einstein",
+    guest_booking: {
+      room_no: "B15",
+      some_array: [2, 5, 6, 3],
     },
   },
   {
-    'guest_type': 'crew',
-    'first_name': 'Jack',
-    'last_name': 'Daniels',
-    'guest_booking': {
-        'room_no': 'B15',
-        'some_array': [2,5,6,3]
+    guest_type: "crew",
+    first_name: "Jack",
+    last_name: "Daniels",
+    guest_booking: {
+      room_no: "B15",
+      some_array: [2, 5, 6, 3],
     },
   },
   {
-    'guest_type': 'guest',
-    'first_name': 'Alan',
-    'last_name': 'Turing',
-    'guest_booking': {
-        'room_no': 'B15',
-        'some_array': [2,5,6,3]
+    guest_type: "guest",
+    first_name: "Alan",
+    last_name: "Turing",
+    guest_booking: {
+      room_no: "B15",
+      some_array: [2, 5, 6, 3],
     },
   },
 ];
 
 function mutateArray(a) {
-    return a;
+  // 1. Update the `mutateArray` function to return `a` as a flattened array
+  a = a.map((obj) => ({
+    guest_type: obj.guest_type,
+    first_name: obj.first_name,
+    last_name: obj.last_name,
+    room_no: obj.guest_booking.room_no,
+    some_array: obj.guest_booking.some_array,
+  }));
+  return a;
 }
 
-$(document).ready(function() {
-    $('#originalArray').html(JSON.stringify(arr, null, 2));
-    $('#resultsArray').html(JSON.stringify(mutateArray(arr), null, 2));
+$(document).ready(function () {
+  $("#originalArray").html(JSON.stringify(arr, null, 2));
+  $("#resultsArray").html(JSON.stringify(mutateArray(arr), null, 2));
 });
